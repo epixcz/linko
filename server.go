d6c6ec499e9248c7e4ea70b938e96dbb731f2413
Ondřej Diok <epix@epix.cz>
2026-05-08 20:09:30 +0200

    4.2

--- server.go
+++ server.go
@@ -49,15 +49,11 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			next.ServeHTTP(w, r)
-			clientIP := r.RemoteAddr
-			if host, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
-				clientIP = host
-			}
 			logger.Info(
 				"Served request",
 				"method", r.Method,
 				"path", r.URL.Path,
-				"client_ip", clientIP,
+				"client_ip", r.RemoteAddr,
 			)
 		})
 	}
